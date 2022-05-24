093a852e29c4078e140e5b57107015b629c0b6ec
ctnohilly <ctnohilly@gmail.com>
2022-05-24 01:16:09 -0400

    Majority of functions handling displaying quiz and creating new screens

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
 var pageContentEl = document.querySelector("#page-content");
 var timeEl = document.querySelector('#time');
+var headerEl = document.querySelector('header');
+headerEl.style.display = 'flex';
 
 var timer = 0; 
 
 // Creating the questions for the quiz
+var currentQuestion = 0;
 var jsQuestions = [];
 var addNewQuestion = function(jsQuestion,answersArr,answerNum){
    jsQuestions.push({
@@ -11,21 +14,15 @@ var addNewQuestion = function(jsQuestion,answersArr,answerNum){
        answers: answersArr,
        answerId: answerNum
    });
-}
-addNewQuestion("Commonly used data types DO NOT include:",["strings","booleans","alerts","nubmers"],3);
-addNewQuestion("The conidion in an if / else statement is enclosed with _____.",["quotes","curly brackets","paranthesis","square brackets"],3);
-
-
-var removeHeader = function(){
-    console.log("In here");
-    console.log(document.querySelector('header'));
-    document.querySelector('header').style.display = "none";// ("display","none");
 };
+addNewQuestion("Commonly used data types DO NOT include:",["strings","booleans","alerts","nubmers"],2);
+addNewQuestion("The condition in an if / else statement is enclosed with _____.",["quotes","curly brackets","paranthesis","square brackets"],1);
+
 
 var createStartQuizScreen = function(){
     pageContentEl.innerHTML = '';
     var headerEl = document.createElement('h2');
-    headerEl.setAttribute('id','question-heading');
+    headerEl.setAttribute('id','title-heading');
     headerEl.textContent = "Coding Quiz Game";
     pageContentEl.appendChild(headerEl);
 
@@ -44,12 +41,54 @@ var createStartQuizScreen = function(){
     divEl.appendChild(btnEl);
     pageContentEl.appendChild(divEl);
     document.querySelector('#start-button').addEventListener("click",startQuiz);
-}
+};
+
+var nextQuestion = function(){
+    if (currentQuestion < jsQuestions.length){
+        document.querySelector('#question-heading').textContent = jsQuestions[currentQuestion].question;
+        for (var i = 0; i < 4; i++){
+           document.querySelector("button[data-answer-id='" + i + "']").textContent = (i+1) + ". " + jsQuestions[currentQuestion].answers[i];
+        }
+    } else {
+        console.log("Quiz is over!");
+        clearInterval(startCountdown);
+        endQuiz();
+    }
+};
 
 var startQuiz = function(){
     startTimer();
-    console.log("Start");
-}
+    pageContentEl.innerHTML = '';
+    var headerEl = document.createElement('h2');
+    headerEl.setAttribute('id','question-heading');
+    pageContentEl.appendChild(headerEl);
+
+    var answersEl = document.createElement('ul');
+    answersEl.setAttribute('id','answers-list');
+    var answerListEl, buttonEl;
+    for(var i = 0; i < 4; i++){
+        answerListEl = document.createElement('li');
+        buttonEl = document.createElement('button');
+        buttonEl.setAttribute('data-answer-id',i);
+        buttonEl.addEventListener('click',answerQuestion);
+        //buttonEl.textContent = "Test";
+        answerListEl.appendChild(buttonEl);
+        answersEl.appendChild(answerListEl);
+    }
+    // answerListEl.addEventListener('click',answerQuestion);
+    pageContentEl.appendChild(answersEl);
+    nextQuestion();
+};
+
+var answerQuestion = function(event){
+    console.log("hello");
+    var answerId = event.target.getAttribute("data-answer-id");
+    if (answerId != jsQuestions[currentQuestion].answerId){
+        wrongAnswer();
+    }
+    currentQuestion++;
+    nextQuestion();
+};
 
 var wrongAnswer = function(){
     timer = timer - 10;
@@ -57,10 +96,13 @@ var wrongAnswer = function(){
         timer = 0;
     }
     timeEl.textContent = timer;
-}
+};
 
+// Timer function - startCountdown needs to be global so other
+// functions may access it and stop the timer
+var startCountdown;
 var startTimer = function () {
-    timer = 15;
+    timer = 25;
     timeEl.textContent = timer;
     var countdown = function () {
         if (timer > 0) {
@@ -70,9 +112,47 @@ var startTimer = function () {
             clearInterval(startCountdown);
         }
     }
-    var startCountdown = setInterval(countdown,1000);
-}
+    startCountdown = setInterval(countdown,1000);
+};
 
-document.querySelector('#highscore-button').addEventListener("click",removeHeader);
+var endQuiz = function(){
+    pageContentEl.innerHTML = '';
+    
+    var headingEl = document.createElement('h2');
+    headingEl.setAttribute('id','quiz-end-heading');
+    headingEl.textContent = "All done!";
+    pageContentEl.appendChild(headingEl);
+    var pEl = document.createElement('p');
+    pEl.textContent = "Your final score is " + timer + ".";
+    pageContentEl.appendChild(pEl);
+
+    var divEl = document.createElement('div');
+    divEl.setAttribute('id','quiz-end');
+    var spanEl = document.createElement('span');
+    spanEl.textContent = "Enter initials:";
+    divEl.appendChild(spanEl);
+    var inputEl = document.createElement('input');
+    inputEl.setAttribute('type','text');
+    inputEl.setAttribute('name','initials');
+    inputEl.setAttribute('placeholder','Initials');
+    inputEl.setAttribute('maxlength','3');
+    divEl.appendChild(inputEl);
+    var buttonEl = document.createElement('button');
+    buttonEl.setAttribute('id','save-score');
+    buttonEl.setAttribute('type','submit');
+    buttonEl.textContent = "Submit";
+    divEl.appendChild(buttonEl);
+    pageContentEl.appendChild(divEl);
+};
+
+var headerToggle = function(){
+    if(headerEl.style.display === "flex"){
+        headerEl.style.display = 'none';
+    } else {
+        headerEl.style.display = 'flex';
+    }
+};
+
+document.querySelector('#highscore-button').addEventListener("click",headerToggle);
 
 createStartQuizScreen();
